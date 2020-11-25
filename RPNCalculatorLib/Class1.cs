562b2a9d9b9ce2692d9d66f55b7d37bc4dcbf5df
Sameer Nazeer <sameernln@gmail.com>
2020-11-25 11:27:10 +0530

    SimpleAdditionLib

--- RPNCalculatorLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RPNCalculatorLib
-{
-    public class Class1
-    {
-    }
-}
---
+++ RPNCalculatorLib/RPNCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace RPNCalculatorLib
+{
+    public class RPNCalculator
+    {
+        public double add(string rpnMathExpression)
+        {
+            return 0.0;
+        }
+    }
+}
